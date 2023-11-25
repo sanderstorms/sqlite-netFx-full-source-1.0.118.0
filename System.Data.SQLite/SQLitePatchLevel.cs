73e89e99e1d7a46e2c862a268ee8401be4459742
Sander Storms <sander.storms.urmond@gmail.com>
2023-11-25 14:26:41 +0100

    initial commit

---
+++ System.Data.SQLite/SQLitePatchLevel.cs
@@ -0,0 +1,16 @@
+/********************************************************
+ * ADO.NET 2.0 Data Provider for SQLite Version 3.X
+ * Written by Joe Mistachkin (joe@mistachkin.com)
+ *
+ * Released to the public domain, use at your own risk!
+ ********************************************************/
+
+using System.Data.SQLite;
+
+///////////////////////////////////////////////////////////////////////////////
+
+[assembly: AssemblySourceId("cc88c047df08636d7ebdc78e746a3ecf8145f434")]
+
+///////////////////////////////////////////////////////////////////////////////
+
+[assembly: AssemblySourceTimeStamp("2023-06-09 21:00:51 UTC")]
